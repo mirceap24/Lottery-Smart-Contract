7cf474ce8a1f706252594621c48fdd47290886b8
mirceamp@DESKTOP-VHG1F6N <mirceamp@DESKTOP-VHG1F6N.localdomain>
2023-02-05 12:37:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a Lottery Smart Contract made with Solidity, JavaScript and Hardhat. 
+
+Functionalities:
+* Users can enter the lottery with ETH 
+* An admin will choose when the lottery is over
+* The lottery will select a verifiable random winner and send the funds to the winner
---
+++ deploy/01-deploy-raffle.js
@@ -0,0 +1,59 @@
+const { getNamedAccounts, deployments, network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock
+
+    if (developmentChains.includes(network.name)) {
+        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait()
+        subscriptionId = transactionReceipt.events[0].args.subId
+        // Fund the subscription
+        // Our mock makes it so we don't actually have to worry about sending fund
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
+    }
+
+    const entranceFee = networkConfig[chainId]["entranceFee"]
+    const gasLane = networkConfig[chainId]["gasLane"]
+    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+    const interval = networkConfig[chainId]["interval"]
+
+    const raffle = await deploy("Raffle", {
+        from: deployer,
+        args: [
+            vrfCoordinatorV2Address,
+            entranceFee,
+            gasLane,
+            subscriptionId,
+            callbackGasLimit,
+            interval,
+        ],
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    // Ensure the Raffle contract is a valid consumer of the VRFCoordinatorV2Mock contract.
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        await vrfCoordinatorV2Mock.addConsumer(subscriptionId, raffle.address)
+    }
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(raffle.address, args)
+    }
+    log("-------------------------------")
+}
+
+module.exports.tags = ["all", "raffle"]
